455de1b1ee95c24449922d54ba8c5196c1a43991
Hyeyeon Hwang <hyehwang@ucdavis.edu>
2018-10-09 17:07:09 -0700

    Add files via upload

--- machine-learning-rett.R
+++ machine-learning-rett.R
@@ -1,47 +1,72 @@
+# caret
+# https://www.analyticsvidhya.com/blog/2016/12/practical-guide-to-implement-machine-learning-with-caret-package-in-r-with-practice-problem/
+
 library(tidyverse)
 library(caret)
 library(randomForest)
 
-
 rettDmrFull <- read.delim("Rett_sig_individual_smoothed_DMR_methylation.txt")
 rettInfo <- read.csv("Rett_sample_info.csv")
 
+# exclude range of columns from 'width' to 'RawDiff', transpose, add diagnosis column
+rettDmr <- rettDmrFull %>% 
+    as.tibble() %>% 
+    select(-(width:RawDiff)) %>%
+    unite(seqId1, seqnames, start, sep = ":") %>%
+    unite(seqId, seqId1, end, sep = "-") %>%
+    # transpose: cols to rows
+    gather(sampleID, values, -seqId) %>% # cols to rows
+    # transpose: rows to cols
+    spread(seqId, values) %>% # rows to cols
+    # add diagnosis column
+    mutate(diagnosis = c("control", "control", "control", 
+                         "rett", "rett", "rett", 
+                         "rett", "rett", "rett", 
+                         "control", "control", "control")) 
+
+rettDmr <- rettDmr %>% select(-sampleID)
+View(dmr)
 
 
-# exclude range of columns from 'width' to 'RawDiff'
-rettDmr <- rettDmrFull %>% as.tibble() %>% select(-(width:RawDiff)) 
-rettDmr
 
-rettDmr <- unite(rettDmr, 
-                 seqId1, 
-                 seqnames, start, 
-                 sep = ":")
-
-rettDmr <- unite(rettDmr, 
-                 seqId, 
-                 seqId1, end, 
-                 sep = "-")
-
-# as.tibble( t(rettDmr) )
-tRettDmr <- rettDmr %>% t() %>% as.tibble()
-
-tRettDmr$diagnosis <- c("diagnosis", "control", "control", "control", 
-                        "rett", "rett", "rett", 
-                        "rett", "rett", "rett", 
-                        "control", "control", "control")
-
-set.seed(5)
-trainIndex <- createDataPartition(tRettDmr$diagnosis, 
+seed <- 9999
+set.seed(seed)
+trainIndex <- createDataPartition(rettDmr$diagnosis, 
                                   p = 0.8,
                                   list = FALSE )
 
-training <- tRettDmr[trainIndex, ]
-testing <- tRettDmr[-trainIndex, ]
+training <- rettDmr[trainIndex, ]
+testing <- rettDmr[-trainIndex, ]
+
+
+fitControl <- trainControl(method = "repeatedcv", # 10-fold cv
+                           number = 10, 
+                           repeats = 10)
+set.seed(seed)
+rf_default <- train( diagnosis ~ ., 
+                     data = training, 
+                     method = "rf", 
+                     trControl = fitControl)
+print(rf_default)
+
+# Checking variable importance for random forest
+vi <- varImp(object = rf_default)
+vi
+# Plotting variable importance for random forest
+vi_plot <- plot( vi, main = "Random Forest - Variable Importance")
+vi_plot
+
+# predictions
+predictions <- predict.train( object = rf_default, testing, type = "raw")
+table(predictions)
+
+# ----------------------------------------------------------------------
 
 cl <- makePSOCKcluster(4)
 registerDoParallel(cl)
+
 # fit a random forest model using ranger 
-rf_fit <- train( as.factor(diagnosis) ~ ., 
+rf_fit <- train( diagnosis ~ ., 
                  data = training, 
                  method = "ranger" )
 stopCluster(cl)
@@ -57,9 +82,6 @@ tRettDmr[ , 1]
 #  rownames_to_column %>% 
 #  gather(var, value, -rowname) %>% 
 #  spread(rowname, value)
-
-
-
 
 
 
