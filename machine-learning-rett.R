046d5e2fb6b88e4e8e7066c5f0e7a2c701959f11
Hyeyeon Hwang <hyehwang@ucdavis.edu>
2018-10-10 11:33:50 -0700

    Add files via upload
    random forest model, warning: missing values in resampled measures

--- machine-learning-rett.R
+++ machine-learning-rett.R
@@ -8,8 +8,9 @@ library(randomForest)
 rettDmrFull <- read.delim("Rett_sig_individual_smoothed_DMR_methylation.txt")
 rettInfo <- read.csv("Rett_sample_info.csv")
 
+# DMR Dataset -------------------------------------------------------------
 # exclude range of columns from 'width' to 'RawDiff', transpose, add diagnosis column
-rettDmr <- rettDmrFull %>% 
+dmr <- rettDmrFull %>% 
     as.tibble() %>% 
     select(-(width:RawDiff)) %>%
     unite(seqId1, seqnames, start, sep = ":") %>%
@@ -18,17 +19,22 @@ rettDmr <- rettDmrFull %>%
     gather(sampleID, values, -seqId) %>% # cols to rows
     # transpose: rows to cols
     spread(seqId, values) %>% # rows to cols
-    # add diagnosis column
-    mutate(diagnosis = c("control", "control", "control", 
-                         "rett", "rett", "rett", 
-                         "rett", "rett", "rett", 
-                         "control", "control", "control")) 
+    # add diagnosis column to front 
+    add_column( diagnosis = factor(c("control", "control", "control", 
+                  "rett", "rett", "rett", 
+                  "rett", "rett", "rett", 
+                  "control", "control", "control")), 
+                .after = 1) 
 
-rettDmr <- rettDmr %>% select(-sampleID)
-View(dmr)
+# remove sampleID column: 1136, 1406, 1711, 1815, 4687, 4852, 
+#                         5020, 5075, 5214, 738, 754, 812
+rettDmr <- dmr %>%
+    select(-sampleID)
 
+# small DMR data set 
+rettDmr <- rettDmr[, 1:100]
 
-
+# Partition data into training and testing --------------------------------
 seed <- 9999
 set.seed(seed)
 trainIndex <- createDataPartition(rettDmr$diagnosis, 
@@ -38,7 +44,7 @@ trainIndex <- createDataPartition(rettDmr$diagnosis,
 training <- rettDmr[trainIndex, ]
 testing <- rettDmr[-trainIndex, ]
 
-
+# Model: Random Forest 10-fold Cross Validation ---------------------------
 fitControl <- trainControl(method = "repeatedcv", # 10-fold cv
                            number = 10, 
                            repeats = 10)
@@ -47,48 +53,27 @@ rf_default <- train( diagnosis ~ .,
                      data = training, 
                      method = "rf", 
                      trControl = fitControl)
-print(rf_default)
 
-# Checking variable importance for random forest
+# predict the outcome on a test set
+rettPredict <- predict(rf_default, testing)
+confusionMatrix(rettPredict, testing$diagnosis)
+
+# Model: Random Forest using ranger ---------------------------------------
+library(ranger)
+rf_fit <- train(diagnosis ~ ., 
+                data = training, 
+                method = "ranger")
+rf_fit
+
+# predict the outcome on a test set
+rangerPredict <- predict(rf_fit, testing)
+confusionMatrix(rangerPredict, testing$diagnosis)
+
+# Model: Stochastic Gradient Boosting -------------------------------------
+
+
+# Variable Importance for Random Forest -----------------------------------
 vi <- varImp(object = rf_default)
 vi
-# Plotting variable importance for random forest
 vi_plot <- plot( vi, main = "Random Forest - Variable Importance")
 vi_plot
-
-# predictions
-predictions <- predict.train( object = rf_default, testing, type = "raw")
-table(predictions)
-
-# ----------------------------------------------------------------------
-
-cl <- makePSOCKcluster(4)
-registerDoParallel(cl)
-
-# fit a random forest model using ranger 
-rf_fit <- train( diagnosis ~ ., 
-                 data = training, 
-                 method = "ranger" )
-stopCluster(cl)
-
-# parallel processing:
-# http://topepo.github.io/caret/parallel-processing.html
-
-tRettDmr[1, c(1, 2)]
-tRettDmr[1, ]
-tRettDmr[ , 1]
-
-#rettDmr $>$
-#  rownames_to_column %>% 
-#  gather(var, value, -rowname) %>% 
-#  spread(rowname, value)
-
-
-
-# perform training using random forest
-rf_classifier <- randomForest(training, 
-                              ntree = 100, 
-                              mtry = 2, 
-                              importance = TRUE)
-rf_classifier
-
