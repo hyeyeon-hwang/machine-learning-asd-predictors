974bf5b59b145143dc36340f0ff572e4e3e81160
hyeyeon-hwang <hyehwang@ucdavis.edu>
2019-02-07 14:26:55 -0800

    added mi and miGrouped, dataType parameter in prepData function

--- asd_predictors_data_prep.R
+++ asd_predictors_data_prep.R
@@ -1,14 +1,15 @@
 # in "asd_predictors_cbdata_prep.R"
 # dmrFull, dmrFullCb, sampleInfo  <- list()
 # library(tidyverse)
-source("asd_predictors_cb_data_prep.R")
+source("asd_predictors_cb_data_prep.R") # dmrFull$cb
+source("asd_predictors_mi_data_prep.R") # dmrFull$mi, sampleInfo$mi
 
 # Read data
-dmrFull$rett <- read.delim("../data/Individual/Rett_sig_individual_smoothed_DMR_methylation.txt", check.names = FALSE)
+dmrFull$rett <- read.delim("../data/Individual/Rett_sig_individual_smoothed_DMR_methylation.txt") #, check.names = FALSE)
 dmrFull$dup <- read.delim("../data/Individual/Dup15q_sig_individual_smoothed_DMR_methylation.txt")
 dmrFull$asd <- read.delim("../data/Individual/ASD_sig_individual_smoothed_DMR_methylation.txt")
 
-dmrFull$plac <- read.delim("../data/Individual/sig_individual_smoothed_DMR_methylation.txt")
+dmrFull$plac <- read.delim("../data/Individual/plac_sig_individual_smoothed_DMR_methylation.txt")
 dmrFullCb$plac <- read.delim("../data/Consensus_background/background_region_individual_smoothed_methylation.txt")
 sampleInfo$plac <- read.csv("../data/Sample_info/sample_info.csv")
 
@@ -17,8 +18,8 @@ sampleInfo$plac <- read.csv("../data/Sample_info/sample_info.csv")
 #' @param dmrFull DMR dataset 
 #' @import tidyverse
 #' @export prepData
-prepData <- function(dmrFull, sampleInfo, cb = FALSE) {
-  if(cb == FALSE) {
+prepData <- function(dmrFull, sampleInfo, dataType = "individual") {
+  if(dataType == "individual") {
     dataPrep1 <- dmrFull %>% 
       as.tibble() %>% 
       # instead of select(-(width:RawDiff)), select(-(width:percentDifference)) for plac
@@ -30,7 +31,7 @@ prepData <- function(dmrFull, sampleInfo, cb = FALSE) {
     
   }
   
-  if(cb == TRUE) {
+  if(dataType == "cb" | dataType == "mi") {
     dataPrep1 <- dmrFull
     seqId <- row.names(dataPrep1)
     dataPrep1 <- dataPrep1 %>%
@@ -52,4 +53,6 @@ dmr$rett <- prepData(dmrFull$rett, sampleInfo$rett)
 dmr$dup <- prepData(dmrFull$dup, sampleInfo$dup)
 dmr$asd <- prepData(dmrFull$asd, sampleInfo$asd)
 dmr$plac <- prepData(dmrFull$plac, sampleInfo$plac)
-dmr$cb <- prepData(dmrFull$cb, sampleInfo$cb, cb = TRUE)
+dmr$cb <- prepData(dmrFull$cb, sampleInfo$cb, dataType = "cb")
+dmr$mi <- prepData(dmrFull$mi, sampleInfo$mi, dataType = "mi")
+dmr$miGrouped <- prepData(dmrFull$mi, sampleInfo$miGrouped, dataType = "mi")
